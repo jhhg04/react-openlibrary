1cfdd6a4619f33d7195881cf5a39eb51407fdcba
JohnHerre <jhhg04@gmail.com>
2020-01-08 16:58:36 -0500

    first commit

---
+++ src/app/index.js
@@ -0,0 +1,12 @@
+import '@babel/polyfill'
+import React, { Component} from 'react'
+import {render} from 'react-dom'
+import App from './App'
+import data from './data.json'
+
+const headings = ['When', 'Who', 'Description']
+
+render(
+    <App data={data} title='OpenLibrary API' headings={headings}/>, 
+    document.getElementById('app')
+)
